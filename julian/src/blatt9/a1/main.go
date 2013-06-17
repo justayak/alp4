8b6628005fc625c271b91691c695267da8fc52a5
justayak <jutanke@googlemail.com>
2013-06-17 15:08:58 +0200

    stff

--- julian/src/blatt9/a1/main.go
+++ julian/src/blatt9/a1/main.go
@@ -2,18 +2,20 @@ package main
 import (
 	"fmt"
 	//"sem"
-	//"queue"
+	"queue"
 	"os/exec"
 	"time"
 	"os"
 	//"strconv"
+	"sync"
 )
 
 var n int = 15
 var count int = 0
-//var queueIn queue.Queue
-//var queueOut queue.Queue
+var queueIn queue.Queue
+var queueOut queue.Queue
 var aufzugUnten bool = true
+var elevator sync.Mutex 
 
 var sekunde = 1000000000
 
@@ -31,47 +33,67 @@ func drawScene(carsIn int , carsOut int , carsParked int ,n int ) string {
 	result := ""
 	for i:=0;i<carsIn;i++ {
 		result += "UU>"
-	}
-	
+	}	
 	if aufzugUnten {
 		result += " [ v ] "
 	}else{
 		result += " [ ^ ] "
-	}
-	
+	}	
 	for i:=0;i<carsOut;i++ {
-		result += "<UU"
-	}
-	
-	result += "__"
-	
+		result += "<U"
+	}	
+	result += "____"	
 	for i:=0;i<carsParked;i++ {
-		result += "[<UU]"
-	}
-	
+		result += "[<U]"
+	}	
 	for i:=0;i<(n-carsParked);i++ {
-		result += "[__]"
+		result += "[_]"
 	}
 	return result
 }
 
 func fill(done chan bool, rendering chan string ){
 	for i:=0;i<10;i++ {		
-		result := drawScene(i, 2, 4, 20)
+		result := drawScene(queueIn.Size(), queueOut.Size(), count, n)		
 		rendering<- result
+		time.Sleep(time.Duration(sekunde))
 	}
 	done <- true
 }
 
+func simulate(){
+	
+}
+
+func simulateElevator(){
+	time.Sleep(time.Duration(sekunde))
+	elevator.Lock()	
+	if aufzugUnten {
+		if queueOut.Size() > 0 && queueIn.Size() == 0 {
+			aufzugUnten = false
+		}
+	}else{
+		if queueIn.Size() > 0 && queueOut.Size() == 0 {
+			aufzugUnten = true
+		}
+	}	
+	elevator.Unlock()
+}
+
 func main(){
-	//queueIn = queue.New()
-	//queueOut = queue.New()
+	queueIn = queue.New()
+	queueOut = queue.New()
+	
+	//in = sem.New(0)
+	//out = sem.New(0)
+	//elevator = make( sem.Semaphore, 0)
 	
 	done := make(chan bool, 0)
 	rendering := make(chan string, 1)
 	
 	go render(rendering)
 	go fill(done,rendering)
+	go simulateElevator()
 	
 	<-done
 	time.Sleep(time.Duration(sekunde))
