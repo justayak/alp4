d0401d2514b71bfa80c9d131e02347366ec6bebe
justayak <justayak@googlemail.com>
2013-06-27 19:21:39 +0200

    qq

--- julian/src/buffer/buffer.go
+++ julian/src/buffer/buffer.go
@@ -4,14 +4,22 @@
 package buffer
 
 type Channel struct { 
-	In, Out chan byte 
+	In, Out chan string 
+	asyncListenerStarted bool
+	n uint
+	async [] chan string 
+	
+	asyncChannels chan chan string
 }
 
 func NewChannel (n uint) *Channel {
 	result:=new( Channel )
-	result.In, result.Out = make(chan byte), make(chan byte)
+	result.n = n
+	result.asyncListenerStarted = false
+	result.asyncChannels = make(chan chan string, n)
+	result.In, result.Out = make(chan string), make(chan string)
 	go func(){
-		buffer:=make([]byte, n)
+		buffer:=make([]string, n)
 		var count, in, out	uint
 		for {
 			if count == 0 {
@@ -37,5 +45,46 @@ func NewChannel (n uint) *Channel {
 	return result
 }
 
-func (x *Channel) Insert(b byte) { x.In <- b }
-func (x *Channel) Get() byte { return <-x.Out }
+func (x *Channel) Insert(b string) { x.In <- b }
+func (x *Channel) Get() string { return <-x.Out }
+
+// Die frage ist irgendwie komisch: ist's so gemeint?
+func (x *Channel) GetAsync2() <-chan string {
+	result:=make(chan string, x.n)
+	x.asyncChannels<-result
+	
+	if !x.asyncListenerStarted {
+		x.asyncListenerStarted = true
+		go func() {
+			for {
+				value:=<-x.Out
+				go func() {
+					current:=<-x.asyncChannels
+					current<-value
+					x.asyncChannels<-current
+				}()
+			}
+		}()
+	}
+	
+	return result
+}
+
+func (x *Channel) GetAsync() <-chan string {
+	result:=make(chan string, x.n)
+	x.async = append(x.async, result)
+	if !x.asyncListenerStarted {
+		x.asyncListenerStarted = true
+		go func() {
+			for {
+				value:=<-x.Out
+				for i:=0; i<len(x.async);i++{					
+					go func() {
+						x.async[i]<-value
+					}()
+				}
+			}
+		}()
+	}
+	return result
+}
