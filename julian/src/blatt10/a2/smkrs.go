fc1ab026fd3f89a2022e7ffcc204c512f53d3c92
justayak <jutanke@googlemail.com>
2013-06-27 13:44:54 +0200

    qqq

---
+++ julian/src/blatt10/a2/smkrs.go
@@ -0,0 +1,19 @@
+package main
+import(
+	"fmt"
+)
+const (
+	None = iota
+	Paper = iota
+	Tobacco = iota
+	Match = iota
+)
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~
+// M A I N
+//~~~~~~~~~~~~~~~~~~~~~~
+func main() {
+	fmt.Println("q")
+}
